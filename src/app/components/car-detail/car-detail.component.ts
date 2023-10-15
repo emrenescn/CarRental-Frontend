15a471b1dc8c27908efde92c4d7596458300dd1a
emre3 <emre3@LAPTOP-L3IAVLVM>
2023-10-15 23:45:41 +0300

    commit 3

--- src/app/components/car-detail/car-detail.component.ts
+++ src/app/components/car-detail/car-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CarDetail } from 'src/app/models/carDetail';
+import { CarDetailService } from 'src/app/services/car-detail.service';
 
 @Component({
   selector: 'app-car-detail',
@@ -6,8 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-detail.component.css']
 })
 export class CarDetailComponent implements OnInit{
-  constructor(){}
+  carDetails:CarDetail[];
+
+  constructor(private carDetailService:CarDetailService,private activatedRoute:ActivatedRoute){}
 ngOnInit(): void {
-  
+  this.activatedRoute.params.subscribe(params=>{
+    if(params["carId"]){
+      this.getCarDetailByCarId(params["carId"]);
+    }
+  })
+}
+getCarDetailByCarId(carId:number){
+this.carDetailService.getCarDetailsByCarId(carId).subscribe(respone=>{
+  this.carDetails=respone.data;
+})
+}
+getImagePath(imagePath:string){
+return "http://localhost:34798/"+imagePath;
 }
 }
--- src/app/services/car-detail.service.ts
+++ src/app/services/car-detail.service.ts
@@ -1,11 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { CarDetail } from '../models/carDetail';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarDetailService {
-
-
-  constructor() { }
+  apiUrl="http://localhost:34798/api/cars/";
+  constructor(private httpClient:HttpClient) { }
+  getCarDetailsByCarId(carId:number):Observable<ListResponseModel<CarDetail>>{
+    let newPath=this.apiUrl+"getcardetailsbycarid?carId="+carId;
+   return this.httpClient.get<ListResponseModel<CarDetail>>(newPath);
+  }
 }
